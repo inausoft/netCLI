9cac33e76bf7237b766715f2ac97a1874983f6e1
Marcin Iwanowski <marcin.r.iwanowski@gmail.com>
2022-08-08 09:05:51 +0200

    Adding support for .net 6 Note: that before when used in .net 6 project - extentions .dll were added from .net 5

---
+++ src/netCLISample.net6/Program.cs
@@ -0,0 +1,13 @@
+﻿//Added to verify the build output for a console project targeting .net 6
+
+namespace netCLISample.net6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
